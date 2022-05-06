feab5b562f1ec227d7ae5bfe56c015f4466d0617
amer1can <amer1can@mail.ru>
2022-05-06 17:47:58 +0300

    menu done

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,48 +1,65 @@
 import {freezeBody, unfreezeBody} from "../../../js/import/freezeBody";
 
-//icon
-const body = document.querySelector('body');
-const icon = document.querySelector('.icon');
-const menu = document.querySelector('.header-menu');
-icon.addEventListener('click', () => {
-	icon.classList.toggle('active')
-	body.classList.toggle('fixed')
+document.addEventListener("DOMContentLoaded", () => {
 
-	if(icon.classList.contains('active')) {
-		menu.classList.add('show');
-		freezeBody();
-	} else {
-		menu.classList.add('action'); //добавляем класс для медленного скрытия меню
-		setTimeout(()=> {
-			menu.classList.remove('action');
-			menu.classList.remove('show')
-		}, 1000)	//скрываем после задержки, такой же как и в css
-		unfreezeBody()
-	}
-})
+
+	//icon
+	const body = document.querySelector('body');
+	const icon = document.querySelector('.icon');
+	const menu = document.querySelector('.header-menu');
+	icon.addEventListener('click', () => {
+		//ничего не делать пока закрывается меню
+		if(!menu.classList.contains('action')) {
+			icon.classList.toggle('active')
+			body.classList.toggle('fixed')
+
+			if(icon.classList.contains('active')) {
+				menu.classList.add('show');
+				freezeBody();
+			} else {
+				menu.classList.add('action'); //добавляем класс для медленного скрытия меню
+				setTimeout(()=> {
+					menu.classList.remove('action');
+					menu.classList.remove('show')
+				}, 1000)	//скрываем после задержки, такой же как и в css
+				unfreezeBody()
+			}
+		}
+	})
 
 // закрытие меню при клике вне элемента
-window.addEventListener('click', e => { // при клике в любом месте окна браузера
-	const target = e.target // находим элемент, на котором был клик
-	if (!target.closest('.header-menu') && !target.closest('.icon')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-		menu.classList.add('action');
-		setTimeout(()=> {
-			menu.classList.remove('action');
-			menu.classList.remove('show')
-			icon.classList.remove('active')
-			body.classList.remove('fixed')
-		}, 1000) // то закрываем окно навигации, удаляя активные классы
-	}
-})
+	window.addEventListener('click', e => { // при клике в любом месте окна браузера
+		const target = e.target // находим элемент, на котором был клик
+		if (!target.closest('.header-menu') && !target.closest('.icon')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+			menu.classList.add('action');
+			setTimeout(()=> {
+				menu.classList.remove('action');
+				menu.classList.remove('show')
+				icon.classList.remove('active')
+				body.classList.remove('fixed')
+			}, 1000) // то закрываем окно навигации, удаляя активные классы
+		}
+	})
 
-// const mediaQuery = window.matchMedia('(max-width: 992px)');
-// function mediaChange(e) {
-// 	//если размер экрана < 992px
-// 	if (e.matches) {
-// 		menu.classList.add('hidden');
-// 	} else {
-// 		menu.classList.remove('hidden');
-// 	}
-// }
-// mediaQuery.addListener(mediaChange);
-// mediaChange(mediaQuery);
+	const headerUi = document.querySelector('.header-ui');
+	const header = document.querySelector('.header-body');
+
+	//
+	if(window.innerWidth < 992) {
+		headerUi.remove();
+		menu.appendChild(headerUi);
+	}
+
+	const mediaQuery = window.matchMedia('(max-width: 992px)');
+	mediaQuery.addEventListener('change', (e) => {
+		if (e.matches) {
+			headerUi.remove();
+			menu.appendChild(headerUi);
+		} else {
+			headerUi.remove();
+			header.appendChild(headerUi);
+		}
+	})
+
+});
+
